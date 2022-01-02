60a2444bb3680707e5dd59d4016cda5728d92a7d
ShibutaniKazuya <ecc2210196@gmail.com>
2022-01-02 20:15:52 +0900

    p

--- OpenSiv3D_0.6.31/Game.cpp
+++ OpenSiv3D_0.6.31/Game.cpp
@@ -17,7 +17,7 @@ void Game::update()
 
 	
 	//Rect{ Arg::center(Cursor::Pos()), DezSamp }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
-	Circle(Cursor::Pos(), DezSamp).draw(Palette::Lightblue);
+	//Circle(Cursor::Pos(), DezSamp).draw(Palette::Lightblue);
 	
 
 	
@@ -25,23 +25,13 @@ void Game::update()
 
 void Game::draw()const
 {
-	//Rect(Arg::center(Scene::Center().x,Scene::Center().y), 960,540).draw();
 	Rect(Arg::topCenter(Scene::Center().x,DezSamp * 2), 960,DezSamp * 6).draw();
 	Rect(Arg::topLeft(0,0), DezSamp).draw();
 	Rect(Arg::topRight(Scene::Width(),0), DezSamp).draw();
 	Rect(Arg::bottomLeft(0,Scene::Height()), DezSamp).draw();
 	Rect(Arg::bottomRight(Scene::Width(),Scene::Height()), DezSamp).draw();
-	/*for (int32 y = 0; y < 50; ++y)
-	{
-		for (int32 x = 0; x < 50; ++x)
-		{
-			if ((x + y) % 2)
-			{
-				Rect{ x * DezSamp, y * DezSamp, DezSamp }.draw(ColorF{ 0.2, 0.3, 0.4 });
-			}
-		}
-	}*/
-	//Circle(Scene::Center().x, Scene::Center().y + DezSamp / 2, DezSamp * 3).draw(Palette::Grey);
+	
+	Circle(Scene::Center().x,Scene::Center().y + DezSamp / 2 , DezSamp * 3).draw(Palette::Grey);
 	for (int32 x = 0; x < 50; ++x)
 	{
 		Line(x * DezSamp, 0, x * DezSamp, Scene::Height()).draw(1,Palette::Cyan);
@@ -50,6 +40,8 @@ void Game::draw()const
 	{
 		Line(0,y*DezSamp,Scene::Width(),y*DezSamp).draw(1, Palette::Cyan);
 	}
+	//Circle(Scene::Center().x,Scene::Center().y , DezSamp * 4).draw(Palette::Gold);
+	//AltCookCircle
 }
 	/*for (int32 y = 0; y < 50; ++y)
 	{
